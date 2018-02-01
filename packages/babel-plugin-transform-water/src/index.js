a0d4274514108e74e2dd7dc51f1a50ec6de71cfe
zhengl <me@zheng.io>
2018-02-01 14:34:55 +0800

    refactor

--- packages/babel-plugin-transform-water/src/index.js
+++ packages/babel-plugin-transform-water/src/index.js
@@ -32,79 +32,76 @@ export default ({ types: t, template }) => {
   const transformAttributesToObjectProperties = attributes =>
     attributes.map(attribute => t.objectProperty(t.identifier(attribute.name.name), attribute.value));
 
-  const JSXOpeningElementVisitor = {
-    JSXAttribute (path, { currentStatement, variableIdentifier }) {
-      let attributeValue;
-      let hydration;
-      const name = path.node.name.name;
-      const value = path.get('value');
+  const mainVisitor = {};
 
-      if (value.isStringLiteral()) {
-        attributeValue = t.stringLiteral(path.node.value.value);
-      } else if (value.isJSXExpressionContainer()) {
-        attributeValue = value.get('expression').node;
-      }
+  const JSXElement = mainVisitor.JSXElement = function (path, { currentStatement, parentIdentifier }) {
+    const targetStatement = currentStatement || path.findParent(parentPath => parentPath.isStatement());
 
-      if (isEventHandlerAttribute(name)) {
-        currentStatement.insertBefore(buildEventHandler({
-          VARIABLE_IDENTIFIER: variableIdentifier,
-          FUNCTION: t.identifier(toEventHandlerIdentifier(name)),
-          HANDLER: attributeValue,
-        }));
-      } else {
-        hydration = buildSetAttribute({
-          VARIABLE_IDENTIFIER: variableIdentifier,
-          ATTRIBUTE: t.stringLiteral(name),
-          VALUE: attributeValue,
-        });
-        currentStatement.insertBefore(hydration);
-      }
+    const openingElement = path.get('openingElement');
+    const variableIdentifier = buildJSXElementVariableIdentifier(path);
+    const tagName = openingElement.get('name').node.name;
 
-      if (value.isJSXExpressionContainer()) {
-        const bindings = value.get('expression').scope.bindings;
-
-        Object.keys(bindings).forEach(binding => {
-          bindings[binding].constantViolations.forEach(violationPath => {
-            violationPath.insertAfter(hydration);
-          });
-        });
-      }
-    },
+    if (isCapitalized(tagName)) {
+      targetStatement.insertBefore(buildCreateElementByFunction({
+        VARIABLE_IDENTIFIER: variableIdentifier,
+        TAG_NAME: t.identifier(tagName),
+        ATTRIBUTES: t.objectExpression(transformAttributesToObjectProperties(openingElement.node.attributes)),
+      }));
+    } else {
+      targetStatement.insertBefore(buildCreateElementByDOM({
+        VARIABLE_IDENTIFIER: variableIdentifier,
+        TAG_NAME: t.stringLiteral(tagName),
+      }));
+    }
+    if (parentIdentifier) {
+      targetStatement.insertBefore(buildAppendChild({
+        PARENT_NODE: parentIdentifier,
+        CHILD_NODE: variableIdentifier,
+      }));
+    }
+    path.traverse(mainVisitor, { parentIdentifier: variableIdentifier, currentStatement: targetStatement });
+    path.replaceWith(variableIdentifier);
   };
 
-  const JSXElementVisitor = {
-    JSXElement (path, { currentStatement, parentIdentifier }) {
-      const openingElement = path.get('openingElement');
-      const variableIdentifier = buildJSXElementVariableIdentifier(path);
-      const tagName = openingElement.get('name').node.name;
+  mainVisitor.JSXAttribute = function (path, { currentStatement, parentIdentifier }) {
+    let attributeValue;
+    let hydration;
+    const name = path.node.name.name;
+    const value = path.get('value');
 
-      if (isCapitalized(tagName)) {
-        currentStatement.insertBefore(buildCreateElementByFunction({
-          VARIABLE_IDENTIFIER: variableIdentifier,
-          TAG_NAME: t.identifier(tagName),
-          ATTRIBUTES: t.objectExpression(transformAttributesToObjectProperties(openingElement.node.attributes)),
-        }));
-      } else {
-        currentStatement.insertBefore(buildCreateElementByDOM({
-          VARIABLE_IDENTIFIER: variableIdentifier,
-          TAG_NAME: t.stringLiteral(tagName),
-        }));
-      }
-      if (parentIdentifier) {
-        currentStatement.insertBefore(buildAppendChild({
-          PARENT_NODE: parentIdentifier,
-          CHILD_NODE: variableIdentifier,
-        }));
-      }
-      if (!isCapitalized(tagName)) {
-        openingElement.traverse(JSXOpeningElementVisitor, { currentStatement, variableIdentifier });
-      }
-      path.get('children').forEach(childPath => childPath.parentPath.traverse(JSXElementVisitor, { currentStatement, parentIdentifier: variableIdentifier }));
-      path.replaceWith(variableIdentifier);
-    },
+    if (value.isStringLiteral()) {
+      attributeValue = t.stringLiteral(path.node.value.value);
+    } else if (value.isJSXExpressionContainer()) {
+      attributeValue = value.get('expression').node;
+    }
+
+    if (isEventHandlerAttribute(name)) {
+      currentStatement.insertBefore(buildEventHandler({
+        VARIABLE_IDENTIFIER: parentIdentifier,
+        FUNCTION: t.identifier(toEventHandlerIdentifier(name)),
+        HANDLER: attributeValue,
+      }));
+    } else {
+      hydration = buildSetAttribute({
+        VARIABLE_IDENTIFIER: parentIdentifier,
+        ATTRIBUTE: t.stringLiteral(name),
+        VALUE: attributeValue,
+      });
+      currentStatement.insertBefore(hydration);
+    }
+
+    if (value.isJSXExpressionContainer()) {
+      const bindings = value.get('expression').scope.bindings;
+
+      Object.keys(bindings).forEach(binding => {
+        bindings[binding].constantViolations.forEach(violationPath => {
+          violationPath.insertAfter(hydration);
+        });
+      });
+    }
   };
 
-  const visitor = {
+  const topVisitor = {
     ArrowFunctionExpression (path) {
       const body = path.get('body');
 
@@ -117,14 +114,11 @@ export default ({ types: t, template }) => {
       }
     },
 
-    JSXElement (path, state) {
-      const currentStatement = path.findParent(parentPath => parentPath.isStatement());
-      path.parentPath.traverse(JSXElementVisitor, { currentStatement });
-    },
+    JSXElement,
   };
 
   return {
     inherits: JSX,
-    visitor,
+    visitor: topVisitor,
   };
 };
