38f129a4fe4c8e83e1eb0d959508ac5e8e51ec76
Zheng Li <me@zheng.io>
2018-02-05 11:39:23 +0800

    added mouse events and form events

--- packages/babel-plugin-transform-water/src/events.js
+++ packages/babel-plugin-transform-water/src/events.js
@@ -1,5 +1,29 @@
 export const events = [
+  // Mouse Events
   'click',
+  'contextMenu',
+  'doubleClick',
+  'drag',
+  'dragEnd',
+  'dragEnter',
+  'dragExit',
+  'dragLeave',
+  'dragOver',
+  'dragStart',
+  'drop',
+  'mouseDown',
+  'mouseEnter',
+  'mouseLeave',
+  'mouseMove',
+  'mouseOut',
+  'mouseOver',
+  'mouseUp',
+
+  // Form Events
+  'change',
+  'input',
+  'invalid',
+  'submit',
 ];
 
 export function toEventHandlerIdentifier (attribute) {
