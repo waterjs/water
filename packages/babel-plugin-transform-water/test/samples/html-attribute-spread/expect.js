816e3f6d8693f2f498881e610559abb3a79fdc2b
Zheng Li <me@zheng.io>
2018-02-11 17:41:52 +0800

    renamed test cases

--- packages/babel-plugin-transform-water/test/samples/html-attribute-spread/expect.js
+++ packages/babel-plugin-transform-water/test/samples/html-attribute-spread/expect.js
@@ -1,4 +1,5 @@
 export default target => {
   expect(target.getAttribute('foo')).toBe('foo');
   expect(target.getAttribute('bar')).toBe('bar');
+  expect(target.getAttribute('wolf')).toBe('wolf');
 };
